3e6b6dbfb36b4e733d8d32db1ccef49da040c398
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-09-16 14:03:03 -0700

    ALL

--- 07-backend/backend.sh
+++ 07-backend/backend.sh
@@ -3,4 +3,6 @@ component=$1
 environment=$2
 sudo dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/NarendraNReddy/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
+#ansible-pull -i localhost, -U https://github.com/NarendraNReddy/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
+
+ansible-pull -i localhost, -U https://github.com/NarendraNReddy1/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
--- 07-backend/main.tf
+++ 07-backend/main.tf
@@ -113,71 +113,71 @@ resource "aws_launch_template" "backend" {
 }
 
 
-# resource "aws_autoscaling_group" "backend" {
-#   name                      = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
-#   max_size                  = 5
-#   min_size                  = 1
-#   health_check_grace_period = 60
-#   health_check_type         = "ELB"
-#   desired_capacity          = 1
-#   target_group_arns = [aws_lb_target_group.backend.arn]
-#   launch_template {
-#     id      = aws_launch_template.backend.id
-#     version = "$Latest"
-#   }
-#   vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+resource "aws_autoscaling_group" "backend" {
+  name                      = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  target_group_arns = [aws_lb_target_group.backend.arn]
+  launch_template {
+    id      = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 
-#   instance_refresh {
-#     strategy = "Rolling"
-#     preferences {
-#       min_healthy_percentage = 50
-#     }
-#     triggers = ["launch_template"]
-#   }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
 
-#   tag {
-#     key                 = "Name"
-#     value               = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
-#     propagate_at_launch = true
-#   }
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
+    propagate_at_launch = true
+  }
 
-#   timeouts {
-#     delete = "15m"
-#   }
+  timeouts {
+    delete = "15m"
+  }
 
-#   tag {
-#     key                 = "Project"
-#     value               = "${var.project_name}"
-#     propagate_at_launch = false
-#   }
-# }
+  tag {
+    key                 = "Project"
+    value               = "${var.project_name}"
+    propagate_at_launch = false
+  }
+}
 
-# resource "aws_autoscaling_policy" "backend" {
-#   name                   = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
-#   policy_type            = "TargetTrackingScaling"
-#   autoscaling_group_name = aws_autoscaling_group.backend.name
+resource "aws_autoscaling_policy" "backend" {
+  name                   = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.backend.name
 
-#   target_tracking_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ASGAverageCPUUtilization"
-#     }
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
 
-#     target_value = 10.0
-#   }
-# }
+    target_value = 10.0
+  }
+}
 
-# resource "aws_lb_listener_rule" "backend" {
-#   listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
-#   priority     = 100 # less number will be first validated
+resource "aws_lb_listener_rule" "backend" {
+  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
+  priority     = 100 # less number will be first validated
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.backend.arn
-#   }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
 
-#   condition {
-#     host_header {
-#       values = ["backend.app-${var.environment}.${var.zone_name}"]
-#     }
-#   }
-# }
+  condition {
+    host_header {
+      values = ["backend.app-${var.environment}.${var.zone_name}"]
+    }
+  }
+}
